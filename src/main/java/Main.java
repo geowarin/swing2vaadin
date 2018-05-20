03d1f99b9e92e5accf2bc14c5020ec48bed89590
geowarin <geowarin@gmail.com>
2018-05-20 17:18:10 +0200

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import spoon.Launcher;
+import spoon.reflect.CtModel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Launcher launcher = new Launcher();
+
+        // path can be a folder or a file
+        // addInputResource can be called several times
+        launcher.addInputResource("/Users/geowarin/dev/bisam/BankApplication/src/main/java");
+
+        // if true, the pretty-printed code is readable without fully-qualified names
+        launcher.getEnvironment().setAutoImports(true); // optional
+
+        // if true, the model can be built even if the dependencies of the analyzed source code are not known or incomplete
+        // the classes that are in the current classpath are taken into account
+        launcher.getEnvironment().setNoClasspath(true); // optional
+
+//        launcher.buildModel();
+//        CtModel model = launcher.getModel();
+//        System.out.println(model);
+
+        launcher.run();
+
+    }
+}
