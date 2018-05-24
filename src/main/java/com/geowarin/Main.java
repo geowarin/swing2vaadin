ae8fbcfe79381b00349948e66b983bbc8bd04a66
geowarin <geowarin@gmail.com>
2018-05-24 21:38:07 +0200

    In place java generation

--- src/main/java/com/geowarin/Main.java
+++ src/main/java/com/geowarin/Main.java
@@ -1,34 +1,43 @@
 package com.geowarin;
 
 import spoon.Launcher;
+import spoon.OutputType;
+import spoon.support.DefaultOutputDestinationHandler;
 import spoon.support.compiler.FileSystemFile;
+import spoon.support.compiler.FileSystemFolder;
+
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
         Launcher launcher = new Launcher();
 
-        // path can be a folder or a file
-        // addInputResource can be called several times
-        launcher.addInputResource("/Users/geowarin/dev/bisam/BankApplication/src/main/java");
+        File inputDir = new File("/Users/geowarin/dev/bisam/BankApplication/src/main/java");
+        File outputDir = new File("/Users/geowarin/dev/bisam/BankApplication/src/main/java");
+//
+        launcher.addInputResource(new FileSystemFolder(inputDir));
+//        launcher.addInputResource("/Users/geowarin/dev/bisam/BankApplication/src/main/java");
 
-        // if true, the pretty-printed code is readable without fully-qualified names
-        launcher.getEnvironment().setAutoImports(true); // optional
+        launcher.getEnvironment().setAutoImports(true);
+        launcher.getEnvironment().setCopyResources(false);
+        launcher.setSourceOutputDirectory(outputDir);
+
+//        DefaultOutputDestinationHandler outputDestinationHandler =
+//                new DefaultOutputDestinationHandler(outputDir, launcher.getEnvironment());
+//        launcher.getEnvironment().setOutputDestinationHandler(outputDestinationHandler);
+//        launcher.getEnvironment().setOutputType(OutputType.NO_OUTPUT);
 
 
         launcher.addTemplateResource(new FileSystemFile("./src/main/java/com/geowarin/AddClickTemplate.java"));
+
         // if true, the model can be built even if the dependencies of the analyzed source code are not known or incomplete
         // the classes that are in the current classpath are taken into account
 //        launcher.getEnvironment().setNoClasspath(true); // optional
-
-//        launcher.buildModel();
-//        CtModel model = launcher.getModel();
-//        System.out.println(model);
 
         launcher.addProcessor(new FieldProcessor());
         launcher.addProcessor(new AssignmentProcessor());
         launcher.addProcessor(new ButtonProcessor());
         launcher.run();
-
     }
 }
